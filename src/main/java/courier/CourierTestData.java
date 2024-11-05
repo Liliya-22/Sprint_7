09f28467684623ef808a335feaefb02b78424e1e
Liliia Mannapova <liliya.mannapova88@gmail.com>
2024-11-05 16:13:08 +0300

    develop

---
+++ src/main/java/courier/CourierTestData.java
@@ -0,0 +1,11 @@
+package courier;
+
+public class CourierTestData {
+    public static final String EXIST_LOGIN = "lolololo";
+    public static final String NOT_EXIST_LOGIN = "ttrtttrtrtrtr44444";
+    public static final String EXIST_PASSWORD = "1234";
+    public static final String NO_EXIST_PASSWORD = "5678";
+    public static final String FIRST_NAME = "lolik";
+    public static final String ID_COURIER_NOT_EXIST = "1";
+
+}
---
+++ src/main/java/orders/OrderTestData.java
@@ -0,0 +1,18 @@
+package orders;
+
+public class OrderTestData {
+    public static final String  FIRST_NAME = "Иван";
+    public static final String  LAST_NAME = "Иванов";
+    public static final String  ADDRESS = "Москва, проспект Вернадского 86, кв 10";
+    public static final String  METRO_STATION = "Юго-Западная";
+    public static final String  PHONE = "89552345678";
+    public static final int  RENT_TIME = 1;
+    public static final String  DELIVERY_DATE = "2024-11-09";
+    public static final String  COMMENT = "Очень жду";
+
+    public static final String COURIER_NOT_EXIST_ID = "1";
+    public static final String [] NEAREST_STATIONS = new String[]{"1","2"};
+    public static final Integer ORDER_LIMIT = 10;
+    public static final Integer ORDER_PAGE = 0;
+    public static final String [] COLOR = new String[]{"BLACK","GREY"};
+}
